7180a54e7e9d70eecc040e02fcbaf4caf11ca780
Ram <amaram@deloitte.com>
2024-06-26 20:06:13 +0530

    consumed mfe code

--- remote-app/src/app/angular-tech/angular-tech-routing.module.ts
+++ remote-app/src/app/angular-tech/angular-tech-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularTechComponent } from './angular-tech.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: AngularTechComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
