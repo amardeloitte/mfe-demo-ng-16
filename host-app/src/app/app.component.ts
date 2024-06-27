74b4382130f569fdba1c8690d2f61e41cafc7348
Ram <amaram@deloitte.com>
2024-06-27 15:27:47 +0530

    setup done

--- host-app/src/app/app.component.ts
+++ host-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataMBusService } from 'data-m-bus';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'host-app';
+
+  constructor(private mbus: DataMBusService){}
+  
+  ngOnInit(): void {
+    this.mbus?.getDataFromRemote().subscribe(res => {
+      console.log(res)
+    })
+  }
 }
--- remote-app/src/app/angular-tech/angular-tech.component.ts
+++ remote-app/src/app/angular-tech/angular-tech.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataMBusService } from 'data-m-bus';
 
 @Component({
   selector: 'app-angular-tech',
   templateUrl: './angular-tech.component.html',
   styleUrls: ['./angular-tech.component.scss']
 })
-export class AngularTechComponent {
+export class AngularTechComponent implements OnInit {
+
+  constructor(private mbus: DataMBusService) {}
+
+  ngOnInit(): void {
+    this.mbus?.setDataFromRemote({data: "remote"});
+  }
 
 }
