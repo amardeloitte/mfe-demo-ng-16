de8f8ae364c7a84c9b78bcd65ecbe083c69f3da6
Ram <amaram@deloitte.com>
2024-07-01 15:55:39 +0530

    local storage added

--- host-app/src/app/app.component.ts
+++ host-app/src/app/app.component.ts
@@ -4,41 +4,46 @@ import { DataMBusService } from 'data-m-bus';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'host-app';
   isNgFav: boolean = false;
-  tech:Array<string> = [];
+  tech: Array<string> = [];
 
-  constructor(private mbus: DataMBusService){}
+  constructor(private mbus: DataMBusService) {}
 
-  ngOnInit(): void
-  {
-    this.mbus?.getDataFromRemote()?.subscribe({
-      next: (res: any) => {
-        this.isNgFav = res?.added;
-        this.tech = [];
-        if(this.isNgFav)
-          {
-            this.tech.push("Angular");
-            setTimeout(() => {
-              this.tech.push("React");
-            }, 3000)
-          }
-          else{
-            this.tech.push("No records");
-          }
+  ngOnInit(): void {
+    // this.mbus?.getDataFromRemote()?.subscribe({
+    //   next: (res: any) => {
+    //     this.isNgFav = res?.added;
+    //     this.tech = [];
+    //     if(this.isNgFav)
+    //       {
+    //         this.tech.push("Angular");
+    //       }
+    //       else{
+    //         this.tech.push("No records");
+    //       }
+    //   }
+    // })
+
+    window.addEventListener('remoteToHostComm', () => {
+      const data = localStorage.getItem('added');
+      this.isNgFav = data === 'true';
+      this.tech = [];
+      if (this.isNgFav) {
+        this.tech.push('Angular');
+      } else {
+        this.tech.push('No records');
       }
-    })
+    });
   }
 
-  removefav(techStr: string)
-  {
-    this.tech = this.tech?.filter(item => item !== techStr);
-    this.tech = this.tech?.length === 0 ? ["No records"]: this.tech;
+  removefav(techStr: string) {
+    this.tech = this.tech?.filter((item) => item !== techStr);
+    this.tech = this.tech?.length === 0 ? ['No records'] : this.tech;
     this.isNgFav = techStr !== 'Angular';
-    !this.isNgFav && this.mbus.setDataFromHost({added: false});
+    !this.isNgFav && this.mbus.setDataFromHost({ added: false });
   }
-
 }
